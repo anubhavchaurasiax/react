d649c1ef5cef45449e0737294a1d1647a406bdc1
manisha chaurasia <anubhav.chaurasia.x@gmail.com>
2026-06-18 14:11:39 +0530

    first react done

--- 01folder/src/App.jsx
+++ 01folder/src/App.jsx
@@ -3,22 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <div id="dad">
-        <h1 id="child1">Eternal</h1>
-        <h2 id="child2">Eternal</h2>
-        <p>We are here for fullfilled our rest of work</p>
-      </div>
-      <div id="hacha">
-
-      </div>
+      <h1>Learning React</h1>
     </>
   )
 }
 
 export default App
-
-
-
-
-
-
