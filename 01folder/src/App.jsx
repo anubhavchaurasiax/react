d4a48d894bad59378a915d788b23342272939414
manisha chaurasia <anubhav.chaurasia.x@gmail.com>
2026-06-18 13:48:48 +0530

    01folder react done

---
+++ 01folder/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+        <h1 id="child1">Eternal</h1>
+        <h2 id="child2">Eternal</h2>
+        <p>We are here for fullfilled our rest of work</p>
+      </div>
+      <div id="hacha">
+
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
